6e69b336dcc8c2469ea9d28cd4b16b533a00376d
YanchukViktoria <139624726+YanchukViktoria@users.noreply.github.com>
2024-11-14 17:33:54 +0200

    f

---
+++ js/script.js
@@ -0,0 +1,117 @@
+/* 1.1 */
+
+const user = {};
+
+user.mood = "happy";
+user.hobby = "skydiving";
+user.premium = "false";
+
+function values(...obj) {
+    for (let el in obj) {
+        console.log(el, obj[el]);
+    }
+}
+
+values(user);
+
+/* 1.2 */
+
+let iterr = 0;
+
+function countProps(...obj) {
+    const arr = obj[0];
+    for (let elem in arr) {
+        console.log(elem);
+    }
+};
+
+countProps(user);
+
+/* 1.3 */
+
+const employees = {
+    Alice: 5,
+    Bob: 8,
+    Charlie: 3,
+    Diana: 10
+};
+
+function findBestEmployee(employees) {
+    return Object.entries(employees).reduce((acc, [name, tasks]) => {
+        return tasks > employees[acc] ? name : acc;
+    }, Object.keys(employees)[0]);
+}
+
+console.log(findBestEmployee(employees));
+
+/* 1.4 */
+
+const employeess = {
+    Alice: 500,
+    Bob: 800,
+    Charlie: 1200,
+};
+function countTotalSalary(employeess) {
+    let total = 0;
+    for (let el in employeess) {
+        total += employeess[el];
+    }
+    return total;
+}
+
+console.log(countTotalSalary(employeess));
+
+/* 1.5 */
+
+let prodvalue = [];
+
+const products = [
+    { name: "Apple", price: 50 },
+    { name: "Banana", price: 30 },
+    { name: "Orange", price: 25 },
+    { name: "Mango" }
+];
+
+const names = [...products.map(({ name }) => name)];
+
+console.log(names);
+
+/* 1.6 */
+
+const productss = [
+    { name: "Apple", price: 50, quantity: 3, },
+    { name: "Banana", price: 30, quantity: 5, },
+    { name: "Orange", price: 25, quantity: 4, },
+    { name: "Mango", price: 60, quantity: 2, }
+];
+
+function countPrice(...elements) {
+    elements.forEach(element => {
+        console.log(element.name, element.price * Number(element.quantity));
+    });
+}
+
+countPrice(...productss)
+
+/* 1.7 */
+
+const account = {
+    balance: 0,
+    transactionHistory: [],
+
+    replenishment(amount) {
+        this.transactionHistory.push({ type: "Deposit", amount, date: new Date() });
+        this.balance = this.transactionHistory.reduce((acc, transaction) => {
+            if (transaction.type === "Deposit") {
+                return acc + transaction.amount;
+            }
+            return acc;
+        }, 0);
+    },
+
+    getTransactionHistory() {
+        this.transactionHistory.forEach(transaction => {
+            console.log(`${transaction.type}: ${transaction.amount} (Дата: ${transaction.date.toLocaleString()})`);
+        });
+    }
+};
